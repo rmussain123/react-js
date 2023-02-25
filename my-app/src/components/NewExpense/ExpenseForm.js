d3843528547118e931e6bd88e3b98e745d1261b9
rmussain123 <49372437+rmussain123@users.noreply.github.com>
2023-02-25 05:42:42 +0530

    Advanced concepts of props

--- my-app/src/components/NewExpense/ExpenseForm.js
+++ my-app/src/components/NewExpense/ExpenseForm.js
@@ -25,7 +25,6 @@ const ExpenseForm = (props) => {
   });
   */
 
-
   const titleChangeHandler = (event) => {
     setEnteredTitle(event.target.value);
     /**
@@ -69,6 +68,7 @@ const ExpenseForm = (props) => {
     };
 
     props.onSaveExpenseData(expenseData);
+    props.onCancel();
     console.log(expenseData);
     setEnteredTitle("");
     setEnteredAmount("");
@@ -105,6 +105,9 @@ const ExpenseForm = (props) => {
         </div>
       </div>
       <div className="new-expense__actions">
+        <button type="button" onClick={props.onCancel}>
+          Cancel
+        </button>
         <button type="submit">Add Expense</button>
       </div>
     </form>
--- my-app/src/components/NewExpense/NewExpense.js
+++ my-app/src/components/NewExpense/NewExpense.js
@@ -1,9 +1,14 @@
-import React from "react";
+import React, { useState } from "react";
 
 import ExpenseForm from "./ExpenseForm";
 import "./NewExpense.css";
 
 const NewExpense = (props) => {
+  const [isEditing, setIsEditing] = useState(false);
+
+  const startEditingHandler = () => {
+    setIsEditing(true);
+  };
   const SaveExpenseDataHandler = (enteredExpenseData) => {
     const expenseData = {
       ...enteredExpenseData,
@@ -11,9 +16,20 @@ const NewExpense = (props) => {
     };
     props.onAddExpense(expenseData);
   };
+  const stopEditingHandler = () => {
+    setIsEditing(false);
+  };
   return (
     <div className="new-expense">
-      <ExpenseForm onSaveExpenseData={SaveExpenseDataHandler} />
+      {!isEditing && (
+        <button onClick={startEditingHandler}>Add New Expense</button>
+      )}
+      {isEditing && (
+        <ExpenseForm
+          onSaveExpenseData={SaveExpenseDataHandler}
+          onCancel={stopEditingHandler}
+        />
+      )}
     </div>
   );
 };
